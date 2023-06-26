0a266208469d192dbfdeb7de4a3e57da9677f344
scramble01 <137489337+scramble01@users.noreply.github.com>
2023-06-26 11:32:55 +0500

    Update water volume.py

--- water volume.py
+++ water volume.py
@@ -1,6 +1,6 @@
 while True:
- print('сейчас вы узнаете свою норму воды в день')
- x = int(input('Напишите свой вес:'))
- a = x*30
- z = ('мл воды в день')
- print(a,z) 
+ print('now you will find out your daily water allowance')
+ user.weight = int(input('Write down your weight:'))
+ water.volume = x*30
+ answer = ('ml of water per day')
+ print(water.volume,answer) 
