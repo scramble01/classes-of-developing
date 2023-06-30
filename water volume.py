6efa003687d9c2d188a241f262e9a9e8b279cc45
scramble <137489337+scramble01@users.noreply.github.com>
2023-06-30 15:37:24 +0500

    Delete water volume.py

--- water volume.py
+++
@@ -1,6 +0,0 @@
-while True:
- print('now you will find out your daily water allowance')
- weight = int(input('Write down your weight:'))
- volume = weight*30
- answer = ('ml of water per day')
- print(volume,answer)
