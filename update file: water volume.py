498079f42b3449b14ceae0378fa1f549353192ea
scramble <137489337+scramble01@users.noreply.github.com>
2023-06-30 20:31:44 +0500

    Delete update file: water volume.py

--- update file: water volume.py
+++
@@ -1,6 +0,0 @@
-while True:
- print('now you will find out your daily water allowance')
- weight = int(input('Write down your weight:'))
- volume = weight * 30
- answer = (f'you need to drink {volume} Ml of water a day')
- print(answer)
