f077dfe1a40a5e5823cf000b75f38af2ce03dc0e
scramble <137489337+scramble01@users.noreply.github.com>
2023-07-13 00:34:38 +0500

    Add files via upload

---
+++ persons .py
@@ -0,0 +1,25 @@
+persons = [
+
+    {'name': 'Иван', 'surname': 'Иванов'},
+
+    {'name': 'Василий', 'surname': 'Васьков'},
+
+    {'name': 'Геннадий', 'surname': 'Генов'},
+    
+]
+
+user_answer = input('Напишите Свое Имя: ')
+
+surnames = []
+for person in persons:
+    if user_answer == (person['name']):
+        surnames.append(person['surname'])
+        print(user_answer,surnames)
+    else:
+        print('Этого Имени нету в базе данных')
+
+
+
+
+
+
