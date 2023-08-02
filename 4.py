5b4ea42a2d989afc7378d9e9ccee948157af144c
scramble <137489337+scramble01@users.noreply.github.com>
2023-08-02 18:28:53 +0500

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,7 @@
-class randomaizer:
+import threading
+from threading import Thread
+
+class Randomizes:
     def __init__(self, random_number, random_N_numbers):
         self.random_number = random_number
         self.random_N_numbers = random_N_numbers
@@ -10,13 +13,22 @@ class randomaizer:
             import random 
             a=random.randint(0,422)
             print(a)
-        
+
         if self.random_N_numbers == 'random N numbers':
-            import random 
-            a = int(input())
-            for y in range(a)
+            import random
+            a = 3
+            for _ in range(a):               
                 x = random.randint(0,422)
                 print(x)
+
+    def runcall(self):
+        if __name__ == '__randoms all__':
+            Thread(target = self.random_number).start()
+            Thread(target = self.random_N_numbers).start()
+
+run = Randomizes()
+run.runcall()
+
 
 
 
