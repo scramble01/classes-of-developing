aa31ce0df8d136e547d3b2f8392f768a362cb10c
scramble <137489337+scramble01@users.noreply.github.com>
2023-08-02 13:45:54 +0500

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,22 @@
+class randomaizer:
+    def __init__(self, random_number, random_N_numbers):
+        self.random_number = random_number
+        self.random_N_numbers = random_N_numbers
+
+
+    def random(self):    
+        
+        if self.random_number == 'random number':
+            import random 
+            a=random.randint(0,422)
+            print(a)
+        
+        if self.random_N_numbers == 'random N numbers':
+            import random 
+            a = int(input())
+            for y in range(a)
+                x = random.randint(0,422)
+                print(x)
+
+
+
