38185541566872ed35826e11b6434049141e5b3c
scramble <137489337+scramble01@users.noreply.github.com>
2025-02-18 21:19:19 +0500

    КВАДРАТНЫЕ УРАВНЕНИЯ.py

--- 1 задание.py
+++ 1 задание.py
@@ -1,23 +1,30 @@
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
-c = int(input("Введите третье число: "))
+while True:
+    a = int(input("Введите первое число: "))
+    b = int(input("Введите второе число: "))
+    c = int(input("Введите третье число: "))
 
-D = b * b - 4 * a * c 
+    D = b * b - 4 * a * c 
 
-if D < 0:
-    print("В этом примере нету корней")
+    if D < 0:
+        print("В этом примере нету корней")
+
+    if D == 0:
+        w = 2 * a
+        x1 = -b / w
     
-else: 
+        print("получился такой корень:")
+        print(x1)
+    if D > 0: 
     
-    import cmath
-    num = D 
-    d1 = (cmath.sqrt(num))
-    w = -b + d1 
-    x1 = w / 2 * a
-    q = -b - d1
-    x2 = q / 2 * a
+        d2 = D**0.5
+        d1 = d2
+        w = -b + d1 
+        x1 = w / 2 * a
+        q = -b - d1
+        x2 = q / 2 * a
     
-    answer = x1,x2
+        answer = x1,x2
     
     print("Получились такие корни: ")
     print(answer)
+    
